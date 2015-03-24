d99fd3d72644f3c3f12ec0d2dbffbffcf86f9fbd
Graham Watson <graham@phantomwatson.com>
2015-03-24 17:00:05 -0400

    Add /login and /logout

--- Config/routes.php
+++ Config/routes.php
@@ -3,6 +3,8 @@ Router::connect('/', array('controller' => 'pages', 'action' => 'home'));
 Router::connect('/glossary', array('controller' => 'pages', 'action' => 'glossary'));
 Router::connect('/calculator', array('controller' => 'calculators', 'action' => 'index'));
 Router::connect('/clear_cache/*', array('controller' => 'pages', 'action' => 'clear_cache'));
+Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
+Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
 
 CakePlugin::routes();
 require CAKE . 'Config' . DS . 'routes.php';
