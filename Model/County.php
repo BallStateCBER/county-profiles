ab250c0a88f0792379afdbc50977629d2a1f130a
Graham Watson <graham@phantomwatson.com>
2015-03-25 15:10:35 -0400

    Edit whitespace

--- Model/County.php
+++ Model/County.php
@@ -4,14 +4,14 @@ class County extends AppModel {
 	public $name = 'County';
 	public $actsAs = array('Containable');
 	public $hasMany = array(
-		'Seat',	
+		'Seat',
 		'City' => array(
 			'order' => 'name'
 		),
 		'Township' => array(
 			'order' => 'name'
 		),
-		'Photo', 
+		'Photo',
 		'CountyDescriptionSource',
 		'CountyWebsite',
 		'TaxDistrict',
@@ -24,7 +24,7 @@ class County extends AppModel {
 		),
 		'State'
 	);
-	
+
 	public function getSlugList() {
 		$cache_key = "countySlugList";
 		if ($cached = Cache::read($cache_key)) {
@@ -37,12 +37,12 @@ class County extends AppModel {
 		));
 		$retval = array();
 		foreach ($counties as $county) {
-			$retval[$county['County']['slug']] = $county['County']['name'];	
+			$retval[$county['County']['slug']] = $county['County']['name'];
 		}
 		Cache::write($cache_key, $retval);
         return $retval;
 	}
-	
+
 	public function getStateId($county_id) {
 		$cache_key = "getStateId($county_id)";
 		if ($cached = Cache::read($cache_key)) {
@@ -56,11 +56,11 @@ class County extends AppModel {
 		if ($result) {
 			$retval = $result['State']['id'];
 			Cache::write($cache_key, $retval);
-			return $retval;	
+			return $retval;
 		}
 		return false;
 	}
-	
+
 	public function getTaxDistricts($county_id) {
 		$cache_key = "getTaxDistricts($county_id)";
 		if ($cached = Cache::read($cache_key)) {
@@ -77,11 +77,11 @@ class County extends AppModel {
 				$tax_districts[] = $tax_district['id'];
 			}
 			Cache::write($cache_key, $tax_districts);
-			return $tax_districts;	
+			return $tax_districts;
 		}
 		return false;
 	}
-	
+
 	public function getSchoolCorps($county_id) {
 		$cache_key = "getSchoolCorps($county_id)";
 		if ($cached = Cache::read($cache_key)) {
@@ -98,11 +98,11 @@ class County extends AppModel {
 				$school_corps[] = $school_corp['id'];
 			}
 			Cache::write($cache_key, $school_corps);
-			return $school_corps;	
+			return $school_corps;
 		}
 		return false;
 	}
-	
+
 	public function getIntro($slug) {
 		$cache_key = "getCountyIntro($slug)";
 		if ($cached = Cache::read($cache_key)) {
@@ -111,10 +111,10 @@ class County extends AppModel {
 		$retval = $this->find('first', array(
 			'conditions' => array('slug' => $slug),
 			'contain' => array(
-				'City', 
-				'Photo', 
-				'Seat', 
-				'CountyDescriptionSource', 
+				'City',
+				'Photo',
+				'Seat',
+				'CountyDescriptionSource',
 				'Township',
 				'CountyWebsite'
 			)
