22745715141f42e90f3e4a305b79d5f22f6181d0
Graham Watson <graham@phantomwatson.com>
2015-11-17 13:20:42 -0500

    Add backup_database()

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -4,6 +4,7 @@ class PagesController extends AppController {
 	public $name = 'Pages';
 	public $uses = array('Segment');
 	public $helpers = array('GoogleCharts.GoogleCharts');
+	public $components = array('DataCenter.Backup');
 
 	public function clear_cache($key = null) {
 		if ($key) {
@@ -14,18 +15,18 @@ class PagesController extends AppController {
 			}
 		} else {
 			if (Cache::clear() && clearCache()) {
-				$this->Flash->success('Cache cleared');	
+				$this->Flash->success('Cache cleared');
 			} else {
-				$this->Flash->success('Error clearing cache');	
+				$this->Flash->success('Error clearing cache');
 			}
 		}
-		
+
 		$this->set(array(
 			'title_for_layout' => 'Clear Cache'
 		));
 		return $this->render('/Pages/home');
 	}
-	
+
 	public function home() {
 		$this->set(array(
 			'title_for_layout' => '',
@@ -33,10 +34,21 @@ class PagesController extends AppController {
 			'example_segment' => $this->Segment->getExample()
 		));
 	}
-	
+
 	public function glossary() {
 		$this->set(array(
 			'title_for_layout' => 'Glossary'
 		));
+	}
+
+	public function backup_database($db_backup_key = null) {
+		$this->Backup->backup_database('County', 'counties', 'counties', $db_backup_key);
+		$tables = array(
+			'rptec_multipliers',
+			'rptemployment_multipliers',
+			'rptibt_multipliers',
+			'rptoutput_multipliers'
+		);
+		$this->Backup->backup_database('County', $tables, 'multipliers', $db_backup_key);
 	}
 }
