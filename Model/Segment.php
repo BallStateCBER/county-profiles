23ce4265892c0fdb16972d6686f0512f696b42c4
Graham Watson <graham@phantomwatson.com>
2015-12-16 16:20:43 -0500

    Catch exception raised by Union county not having any school corps
    When this exception is thrown, now the corresponding 'high school
    graduation rates' section is just not shown. In debug mode, an error
    message is shown at the top of the screen.

--- Model/Segment.php
+++ Model/Segment.php
@@ -1,5 +1,5 @@
 <?php
-/* A "segment" is one of the sections of a county profile, 
+/* A "segment" is one of the sections of a county profile,
  * such as "household income", "educational attainment", etc.
  * These have descriptions, usually charts, and tables.
  */
@@ -11,7 +11,7 @@ App::uses('Source', 'Model');
 class Segment extends AppModel {
 	public $name = 'Segment';
 	public $actsAs = array('Containable');
-	
+
 	public function getAllForTab($tab, $county) {
 		$cache_key = "getAllSegmentsForTab($tab, $county)";
 		if (! $segments = Cache::read($cache_key)) {
@@ -25,26 +25,33 @@ class Segment extends AppModel {
 		$rekeyed_segments = array();
 		foreach ($segments as $segment) {
 			$segment_name = $segment['Segment']['name'];
-			$rekeyed_segments[$segment_name] = array_merge($segment, $this->getSegment($segment_name, $county));
+            try {
+                $contents = $this->getSegment($segment_name, $county);
+                $rekeyed_segments[$segment_name] = array_merge($segment, $contents);
+            } catch (InternalErrorException $e) {
+                if (Configure::read('debug')) {
+                    echo '<p class="error_message">Caught exception: '.$e->getMessage().'</p>';
+                }
+            }
 		}
 		return $rekeyed_segments;
 	}
 
 	public function getSegment($segment, $county) {
 		$SegmentData = new SegmentData();
-		
+
 		// Makes available $data, $source_ids, $structure, $subsegments, and $subsegments_display
 		extract($SegmentData->getData($segment, $county));
-		
+
 		// Makes available $dates, $locations, and $categories
 		extract($SegmentData->segmentParams);
-		
+
 		$output = array('data' => $data);
 		if ($data) {
 			$more_output = $this->__getOutput($SegmentData, $segment, $data, $source_ids, $structure);
 			$output = array_merge($output, $more_output);
 		}
-		
+
 		// Load up subsegments with all of the same output as segments
 		$output['subsegments'] = array();
 		if (! empty($subsegments)) {
@@ -53,24 +60,24 @@ class Segment extends AppModel {
 				$output['subsegments'][$ss_name] = array_merge(
 					array('title' => $title),
 					$this->getSegment($ss_name, $county)
-				); 
+				);
 			}
-			
+
 			// Determine the parent segment's sources by grouping the subsegment sources
 			$combined_sources = array();
 			foreach ($output['subsegments'] as $ss_name => $attributes) {
 				foreach ($attributes['sources'] as $source) {
 					if (! in_array($source, $combined_sources)) {
-						$combined_sources[] = $source; 
+						$combined_sources[] = $source;
 					}
 				}
 			}
 			$output['sources'] = $combined_sources;
 		}
-		
+
 		return $output;
 	}
-	
+
 	private function __getOutput($SegmentData, $segment, $data = array(), $source_ids = array(), $structure = array()) {
 		$Source = new Source();
 		$Chart = new Chart();
@@ -81,7 +88,7 @@ class Segment extends AppModel {
 			'Table' => $Table->getTable($segment, $data, $SegmentData->segmentParams, $structure)
 		);
 	}
-	
+
 	public function getExample() {
 		return $this->getSegment('econ_unemployment', 'Marion');
 	}
