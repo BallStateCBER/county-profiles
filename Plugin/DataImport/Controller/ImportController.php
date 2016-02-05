1c4e84bac2782a62ce8f42434230213e873d004e
Graham Watson <graham@phantomwatson.com>
2016-02-05 16:06:43 -0500

    Update directory for import files

--- Plugin/DataImport/Controller/ImportController.php
+++ Plugin/DataImport/Controller/ImportController.php
@@ -3,13 +3,13 @@ App::uses('AppController', 'Controller');
 class ImportController extends AppController {
 	public $name = 'Import';
 	public $uses = array('DataImport.Import');
-	
-	public $importDirectory = 'C:\\Users\\gtwatson\\Documents\\Projects, CBER\\County Profiles\\Data to Import';
-	
+
+	public $importDirectory = 'C:\\Users\\gtwatson\\Box Sync\\Projects\\CBER\\County Profiles (gtwatson@bsu.edu)\\Data to Import';
+
 	function beforeFilter() {
 		parent::beforeFilter();
 		if (stripos($_SERVER['SERVER_NAME'], 'localhost') === false) {
-			throw new InternalErrorException("Importing can only take place on the development server.");	
+			throw new InternalErrorException("Importing can only take place on the development server.");
 		}
 	}
 
@@ -28,7 +28,7 @@ class ImportController extends AppController {
 		$auto = isset($_GET['auto']) ? $_GET['auto'] : false;
 		$page = isset($_GET['page']) ? $_GET['page'] : false;
 		extract($this->Import->processDataFile($params, $auto, $page));
-		
+
 		// Check and see if these variables could be trimmed
 		$this->set(compact('file', 'import_results', 'page'));
 		$this->set($params);
